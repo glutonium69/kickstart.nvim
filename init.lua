9db4868f34c7eaa2c99d23510b17bc3ef11cbfc8
ChuzaWick420 <zaiddadev@gmail.com>
2024-10-06 23:43:58 +0500

    changed the engine to lualatex but it still doesn't work on my machine for some reason

--- init.lua
+++ init.lua
@@ -262,6 +262,13 @@ require('lazy').setup({
     init = function()
       -- VimTeX configuration goes here, e.g.
       vim.g.vimtex_compiler_method = 'latexmk'
+      vim.g.vimtex_compiler_latexmk = {
+        options = {
+          '-lualatex', -- Use lualatex as the engine
+          '-synctex=1', -- SyncTeX support
+          '-interaction=nonstopmode', -- Continue compiling even if errors are found
+        },
+      }
       vim.g.vimtex_view_method = 'general'
     end,
   },
