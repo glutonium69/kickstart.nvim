d8bbaa9a1fd2384737f0c9842e1d2cbcd53c6391
ChuzaWick420 <zaiddadev@gmail.com>
2024-10-06 22:45:16 +0500

    Added: VimTex plugin

--- init.lua
+++ init.lua
@@ -255,7 +255,16 @@ require('lazy').setup({
       },
     },
   },
-
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_compiler_method = 'latexmk'
+      vim.g.vimtex_view_method = 'general'
+    end,
+  },
   { 'wakatime/vim-wakatime', lazy = false },
   {
     'andweeb/presence.nvim',
