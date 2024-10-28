7c2040d8c0312c2ca10eacbe6c77072f3a978322
ChuzaWick420 <zaiddadev@gmail.com>
2024-10-28 20:35:20 +0500

    Added and Enabled: friendly snippets

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -55,6 +55,8 @@ require('lazy').setup({
 
   require 'custom/plugins/discord_rich_presence',
 
+  require 'custom/plugins/snippers',
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
